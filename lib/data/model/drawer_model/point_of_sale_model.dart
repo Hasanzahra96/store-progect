6a58dfe82b3ecf95130b9c9147f4a9d5db461e49
HASAN_ZAHRA <h.96.n.sy@gmail.com>
2026-08-02 21:51:31 +0300

    add points of sale feature with controller, model, and UI components

---
+++ lib/data/model/drawer_model/point_of_sale_model.dart
@@ -0,0 +1,17 @@
+class PointOfSaleModel {
+  final String? imagePath;
+  final String? title;
+  final String? subTitle;
+  final String? firstTime;
+  final String? lastTime;
+  final String? location;
+  final String? distans;
+  PointOfSaleModel(
+      {this.imagePath,
+      this.title,
+      this.subTitle,
+      this.firstTime,
+      this.lastTime,
+      this.location,
+      this.distans});
+}
